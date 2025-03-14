3b48b12f60f7985cf58fffa38f8383412a8984c1
samidgit <samijalene@gmail.com>
2025-03-14 06:28:38 -0400

    netlify

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,6 +10,7 @@ import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+// protected
 const stripePromise = loadStripe(
   "pk_test_51R0aqSG0Q2APUPC3H7QUAWZDpywup0ignn8C32YqSNz0h5Cyj5wSlNOTWulTYvvLKykE5drXnzyLk7776npDUAh700MzaQitba"
 );
